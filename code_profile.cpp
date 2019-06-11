baa1045b8c1d44ac234cacc13f1c53fc19080eee
Mikhail Lundin <mlyundin@gmail.com>
2019-06-11 11:34:07 +0300

    Add profile tool

---
+++ code_profile.cpp
@@ -0,0 +1,31 @@
+#include "code_profile.h"
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+using namespace chrono;
+
+TotalDuration::TotalDuration(const string& msg): message(msg + ": "), value(0) {}
+
+TotalDuration::~TotalDuration() {
+    cerr << message << duration_cast<milliseconds>(value).count() << " ms"<< endl;
+}
+
+
+AddDuration::AddDuration(steady_clock::duration& dest): add_to(dest), start(steady_clock::now()) {}
+
+AddDuration::AddDuration(TotalDuration& dest): AddDuration(dest.value) {}
+
+AddDuration::~AddDuration() {
+    add_to += steady_clock::now() - start;
+}
+
+LogDuration::LogDuration(const string& msg): message(msg + ": "), start(steady_clock::now()) {}
+
+LogDuration::~LogDuration() {
+  auto finish = steady_clock::now();
+  auto dur = finish - start;
+  cerr << message
+     << duration_cast<milliseconds>(dur).count()
+     << " ms" << endl;
+}
---
+++ code_profile.h
@@ -0,0 +1,39 @@
+#pragma once
+#include<string>
+#include<chrono>
+
+struct TotalDuration {
+    std::string message;
+    std::chrono::steady_clock::duration value;
+    explicit TotalDuration(const std::string& msg = "");
+    ~TotalDuration();
+};
+
+class AddDuration {
+public:
+    explicit AddDuration(std::chrono::steady_clock::duration& dest);
+    explicit AddDuration(TotalDuration& dest);
+    ~AddDuration();
+private:
+    std::chrono::steady_clock::duration& add_to;
+    std::chrono::steady_clock::time_point start;
+};
+
+#define ADD_DURATION(value) \
+AddDuration UNIQ_ID(__LINE__){value};
+
+class LogDuration {
+public:
+  explicit LogDuration(const std::string& msg = "");
+
+  ~LogDuration();
+private:
+  std::string message;
+  std::chrono::steady_clock::time_point start;
+};
+
+#define UNIQ_ID_IMPL(lineno) _a_local_var_##lineno
+#define UNIQ_ID(lineno) UNIQ_ID_IMPL(lineno)
+
+#define LOG_DURATION(message) \
+  LogDuration UNIQ_ID(__LINE__){message};
