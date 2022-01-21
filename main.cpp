b259e125980e4eeefa4bc43ed51156bfca4955e0
Mikhail Lyundin <Mikhail.Lyundin@nordigy.ru>
2022-01-21 22:09:28 +0300

    Code refactoring

--- main.cpp
+++ main.cpp
@@ -12,106 +12,113 @@
 using namespace busdb;
 using namespace Json;
 
-
-void ReadSettings(const Object& in_data, DataBase& db) {
-    if(in_data.count("routing_settings")) {
-         const auto& settings = in_data.at("routing_settings").AsObject();
-         db.SetSettings({settings.at("bus_wait_time").AsInt(),
-                         settings.at("bus_velocity").AsInt()});
-     }
-}
-
-template<class RequestType>
-auto ReadRequests(std::istream& in_stream = std::cin) {
-    std::list<std::unique_ptr<RequestType>> requests;
-    const size_t request_count = ReadNumberOnLine<size_t>(in_stream);
-
-    for (size_t i = 0; i < request_count; ++i) {
-        std::string request_str;
-        getline(in_stream, request_str);
-        if (auto request = ParseRequest<RequestType>(request_str)) {
-            requests.push_back(move(request));
-        }
-    }
-    return requests;
-}
-
-template<class RequestType>
-auto ReadJsonRequests(const Array& in_data) {
-    LOG_DURATION("ReadJsonRequests");
-    std::list<std::unique_ptr<RequestType>> requests;
-
-    for (auto& item : in_data) {
-        if (auto request = ParseJsonRequest<RequestType>(item)) {
-            requests.push_back(move(request));
+namespace {
+    void ReadSettings(const Object &in_data, DataBase &db) {
+        if (in_data.count("routing_settings")) {
+            const auto &settings = in_data.at("routing_settings").AsObject();
+            db.SetSettings({settings.at("bus_wait_time").AsInt(),
+                            settings.at("bus_velocity").AsInt()});
         }
     }
 
-    return requests;
-}
+    template<class RequestType>
+    auto ReadRequests(std::istream &in_stream = std::cin) {
+        std::list<std::unique_ptr<RequestType>> requests;
+        const size_t request_count = ReadNumberOnLine<size_t>(in_stream);
 
-template <class RequestContainer>
-void ProcessModifyRequest(const RequestContainer& requests, DataBase& db) {
-    {
-        TotalDuration process(" Process Modify Request");
-        for (const auto& request_holder : requests) {
-            ADD_DURATION(process);
-            request_holder->Process(db);
+        for (size_t i = 0; i < request_count; ++i) {
+            std::string request_str;
+            getline(in_stream, request_str);
+            if (auto request = ParseRequest<RequestType>(request_str)) {
+                requests.push_back(move(request));
+            }
         }
-    }
-    {
-        LOG_DURATION("BuildRoutes");
-        db.BuildRoutes();
+        return requests;
     }
 
-}
+    template<class RequestType>
+    auto ReadJsonRequests(const Array &in_data) {
+        LOG_DURATION("ReadJsonRequests");
+        std::list<std::unique_ptr<RequestType>> requests;
 
-template<class RequestContainer> auto ProcessReadRequests(
-        const RequestContainer& requests, const DataBase& db) {
-    LOG_DURATION("ProcessReadRequests");
-    std::list<std::unique_ptr<AbstractData>> responses;
-    for (const auto& request: requests) {
-        responses.push_back(request->Process(db));
-    }
-    return responses;
-}
+        for (auto &item: in_data) {
+            if (auto request = ParseJsonRequest<RequestType>(item)) {
+                requests.push_back(move(request));
+            }
+        }
 
-template<class ResponseContainer>
-auto ResponsesToDocument(const ResponseContainer& responses) {
-    LOG_DURATION("ResponsesToDocument");
-    auto json_responses = Array();
-    json_responses.reserve(responses.size());
-
-    for (const auto& response : responses) {
-        json_responses.push_back(response->toJson());
+        return requests;
     }
 
-    return Document(json_responses);
-}
+    template<class RequestContainer>
+    void ProcessModifyRequest(const RequestContainer &requests, DataBase &db) {
+        {
+            TotalDuration process(" Process Modify Request");
+            for (const auto &request_holder: requests) {
+                ADD_DURATION(process);
+                request_holder->Process(db);
+            }
+        }
+        {
+            LOG_DURATION("BuildRoutes");
+            db.BuildRoutes();
+        }
 
-template<class ResponseContainer>
-void PrintResponses(const ResponseContainer& responses,
-                    std::ostream& out_stream = std::cout) {
-    for (const auto& response : responses) {
-        out_stream << *response << std::endl;
+    }
+
+    template<class RequestContainer>
+    auto ProcessReadRequests(
+            const RequestContainer &requests, const DataBase &db) {
+        LOG_DURATION("ProcessReadRequests");
+        std::list<std::unique_ptr<AbstractData>> responses;
+        for (const auto &request: requests) {
+            responses.push_back(request->Process(db));
+        }
+        return responses;
+    }
+
+    template<class ResponseContainer>
+    auto ResponsesToDocument(const ResponseContainer &responses) {
+        LOG_DURATION("ResponsesToDocument");
+        auto json_responses = Array();
+        json_responses.reserve(responses.size());
+
+        for (const auto &response: responses) {
+            json_responses.push_back(response->toJson());
+        }
+
+        return Document(json_responses);
+    }
+
+    template<class ResponseContainer>
+    void PrintResponses(const ResponseContainer &responses,
+                        std::ostream &out_stream = std::cout) {
+        for (const auto &response: responses) {
+            out_stream << *response << std::endl;
+        }
     }
 }
 
 int main() {
-
     LOG_DURATION("Total")
     DataBase db;
     std::ostream& out = std::cout;
 #ifdef DEBUG
-    std::ifstream ifs("test_input.txt");
+    std::ifstream ifs("input2.json");
     std::istream& in = ifs;
 #else
     std::istream& in = std::cin;
 #endif
 
-    std::cout.precision(6);
-    if (auto is_json = true; is_json) {
-        auto in_data = Load(in);
+    out.precision(6);
+#ifdef PLAN_TEXT
+    const auto modify_requests = ReadRequests<ModifyRequest>(in);
+    const auto read_requests = ReadRequests<ReadRequest>(in);
+
+    ProcessModifyRequest(modify_requests, db);
+    PrintResponses(ProcessReadRequests(read_requests, db), out);
+#else
+    auto in_data = Load(in);
         auto& requests = in_data.GetRoot().AsObject();
         ReadSettings(requests, db);
 
@@ -121,21 +128,14 @@ int main() {
                 requests.at("stat_requests").AsArray());
 
         ProcessModifyRequest(modify_requests, db);
-
         auto doc = ResponsesToDocument(ProcessReadRequests(read_requests, db));
 
-#ifdef DEBUG
-        std::ifstream output("test_output.txt");
-        assert(EqualWithSkip(doc, Load(output), std::unordered_set<std::string>{"items"}));
-#endif
+//#ifdef DEBUG
+//        std::ifstream output("test_output.txt");
+//        assert(EqualWithSkip(doc, Load(output), std::unordered_set<std::string>{"items"}));
+//#endif
         Save(doc, out);
-    } else {
-        const auto modify_requests = ReadRequests<ModifyRequest>(in);
-        const auto read_requests = ReadRequests<ReadRequest>(in);
-
-        ProcessModifyRequest(modify_requests, db);
-        PrintResponses(ProcessReadRequests(read_requests, db), out);
-    }
+#endif
 
     return 0;
 }
